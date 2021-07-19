ae6cedf032ed79e3b8267def69f177731c43a7af
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-19 11:30:04 -0500

    Instalando y Activando un Service Worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Cuando se instala el Service Worker
+self.addEventListener('install', e => {
+    console.log('Instalando el Service Worker');
+    console.log(e);
+})
+
+// Activar el Service Worker
+self.addEventListener('activate', e => {
+    console.log('Service Worker activado');
+    
+    console.log(e);
+})
