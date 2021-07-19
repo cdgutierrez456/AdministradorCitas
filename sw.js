18282b49783aa9ca77420d6cb9f34c95378dc1ec
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-19 15:20:28 -0500

    Cacheando archivos principales

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
+const nombreCache = 'apv-v1';
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/funciones.js',
+    '/js/clases/App.js',
+    '/js/clases/UI.js'
+];
+
 // Cuando se instala el Service Worker
 self.addEventListener('install', e => {
     console.log('Instalando el Service Worker');
-    console.log(e);
+
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then(cache => {
+                console.log('cacheando');
+                cache.addAll(archivos)
+            })
+    )
+
 })
 
 // Activar el Service Worker
@@ -13,7 +33,7 @@ self.addEventListener('activate', e => {
 
 // Evento fetch para descargar archivos estaticos
 self.addEventListener('fetch', e => {
-    console.log('Fetch...', e);
+    // console.log('Fetch...', e);
 })
 
 
